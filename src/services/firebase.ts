4e3a3fabb2be50163c2d479f64e66436e8823379
meneses <vitormeneses87@gmail.com>
2023-12-28 12:06:58 -0300

    feat: add config firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+const firebaseConfig = {
+  apiKey: process.env.firebase_apiKey,
+  authDomain: process.env.firebase_authDomain,
+  projectId: process.env.firebase_projectId,
+  storageBucket: process.env.firebase_storageBucket,
+  messagingSenderId: process.env.firebase_messagingSenderId,
+  appId: process.env.firebase_appId,
+  measurementId: process.env.firebase_measurementId,
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
